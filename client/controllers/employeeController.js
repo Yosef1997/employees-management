cea6b355c0bf132803cf450019ee33ae03338188
JanBu <yosefsitumorang97@gmail.com>
2025-10-03 00:24:53 +0700

    chore(cleanup): remove unused comments from code

--- client/controllers/employeeController.js
+++ client/controllers/employeeController.js
@@ -12,7 +12,6 @@ app.controller(
     $scope.departments = []
     $scope.searchText = ''
 
-    // Pagination data
     $scope.pagination = {
       total: 0,
       page: 1,
@@ -20,14 +19,13 @@ app.controller(
       totalPage: 0,
     }
 
-    // Load Employees (dengan pagination & search)
     $scope.loadEmployees = function (page = 1) {
       EmployeeService.getAll(
         page,
         $scope.pagination.limit,
         $scope.searchText
       ).then(function (res) {
-        $scope.employees = res.data.data // daftar karyawan
+        $scope.employees = res.data.data
         $scope.pagination.total = res.data.total
         $scope.pagination.page = res.data.page
         $scope.pagination.limit = res.data.limit
@@ -35,21 +33,18 @@ app.controller(
       })
     }
 
-    // Load Departments
     $scope.loadDepartments = function () {
       DepartmentService.getAll().then(function (res) {
         $scope.departments = res.data
       })
     }
 
-    // By ID (detail employee untuk edit)
     if ($routeParams.id) {
       EmployeeService.getById($routeParams.id).then(function (res) {
         $scope.employee = res.data
       })
     }
 
-    // Tambah
     $scope.addEmployee = function () {
       if (!$scope.employee.Name || $scope.employee.Salary <= 0) {
         alert('Nama tidak boleh kosong dan gaji harus > 0')
@@ -60,7 +55,6 @@ app.controller(
       })
     }
 
-    // Update
     $scope.updateEmployee = function () {
       EmployeeService.update($routeParams.id, $scope.employee).then(
         function () {
@@ -69,7 +63,6 @@ app.controller(
       )
     }
 
-    // Hapus
     $scope.deleteEmployee = function (id) {
       if (confirm('Yakin hapus data?')) {
         EmployeeService.remove(id).then(function () {
@@ -78,7 +71,6 @@ app.controller(
       }
     }
 
-    // Init
     $scope.loadEmployees()
     $scope.loadDepartments()
   }
