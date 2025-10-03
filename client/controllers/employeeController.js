eac6244bf08be57e73d15fd0e078abfe1c9aeee2
JanBu <yosefsitumorang97@gmail.com>
2025-10-03 11:16:56 +0700

    feat(controller): menambahkan state loading dan alert box success/error saat proses CRUD employee

--- client/controllers/employeeController.js
+++ client/controllers/employeeController.js
@@ -11,7 +11,8 @@ app.controller(
     $scope.employee = {}
     $scope.departments = []
     $scope.searchText = ''
-
+    $scope.loading = false
+    $scope.alert = { message: '', type: '' }
     $scope.pagination = {
       total: 0,
       page: 1,
@@ -19,24 +20,45 @@ app.controller(
       totalPage: 0,
     }
 
+    function showAlert(message, type) {
+      $scope.alert.message = message
+      $scope.alert.type = type
+      setTimeout(() => {
+        $scope.$apply(() => ($scope.alert.message = ''))
+      }, 3000)
+    }
+
     $scope.loadEmployees = function (page = 1) {
-      EmployeeService.getAll(
-        page,
-        $scope.pagination.limit,
-        $scope.searchText
-      ).then(function (res) {
-        $scope.employees = res.data.data
-        $scope.pagination.total = res.data.total
-        $scope.pagination.page = res.data.page
-        $scope.pagination.limit = res.data.limit
-        $scope.pagination.totalPage = res.data.totalPage
-      })
+      $scope.loading = true
+      $scope.pagination.page = page
+      EmployeeService.getAll(page, $scope.pagination.limit, $scope.searchText)
+        .then(function (res) {
+          $scope.employees = res.data.data
+          $scope.pagination.total = res.data.total
+          $scope.pagination.page = res.data.page
+          $scope.pagination.limit = res.data.limit
+          $scope.pagination.totalPage = res.data.totalPage
+        })
+        .catch(function (err) {
+          showAlert('Gagal memuat data karyawan', 'error')
+        })
+        .finally(function () {
+          $scope.loading = false
+        })
     }
 
     $scope.loadDepartments = function () {
-      DepartmentService.getAll().then(function (res) {
-        $scope.departments = res.data
-      })
+      $scope.loading = true
+      DepartmentService.getAll()
+        .then(function (res) {
+          $scope.departments = res.data
+        })
+        .catch(function (err) {
+          showAlert('Gagal memuat data departemen', 'error')
+        })
+        .finally(function () {
+          $scope.loading = false
+        })
     }
 
     if ($routeParams.id) {
@@ -47,27 +69,52 @@ app.controller(
 
     $scope.addEmployee = function () {
       if (!$scope.employee.Name || $scope.employee.Salary <= 0) {
-        alert('Nama tidak boleh kosong dan gaji harus > 0')
+        showAlert('Nama tidak boleh kosong dan gaji harus > 0', 'error')
         return
       }
-      EmployeeService.create($scope.employee).then(function () {
-        $location.path('/employees')
-      })
+      $scope.loading = true
+      EmployeeService.create($scope.employee)
+        .then(function () {
+          showAlert('Karyawan berhasil ditambahkan', 'success')
+          $location.path('/employees')
+        })
+        .catch(function () {
+          showAlert('Gagal menambahkan karyawan', 'error')
+        })
+        .finally(function () {
+          $scope.loading = false
+        })
     }
 
     $scope.updateEmployee = function () {
-      EmployeeService.update($routeParams.id, $scope.employee).then(
-        function () {
+      $scope.loading = true
+      EmployeeService.update($routeParams.id, $scope.employee)
+        .then(function () {
+          showAlert('Karyawan berhasil diupdate', 'success')
           $location.path('/employees')
-        }
-      )
+        })
+        .catch(function () {
+          showAlert('Gagal mengupdate karyawan', 'error')
+        })
+        .finally(function () {
+          $scope.loading = false
+        })
     }
 
     $scope.deleteEmployee = function (id) {
       if (confirm('Yakin hapus data?')) {
-        EmployeeService.remove(id).then(function () {
-          $scope.loadEmployees()
-        })
+        $scope.loading = true
+        EmployeeService.remove(id)
+          .then(function () {
+            showAlert('Karyawan berhasil dihapus', 'success')
+            $scope.loadEmployees($scope.pagination.page)
+          })
+          .catch(function () {
+            showAlert('Gagal menghapus karyawan', 'error')
+          })
+          .finally(function () {
+            $scope.loading = false
+          })
       }
     }
 
